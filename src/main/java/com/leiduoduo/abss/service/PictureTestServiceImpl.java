697fade1960f4430c4af0cda7701ac31eafdef07
xiaogudamowang <60064087+xiaogudamowang@users.noreply.github.com>
2021-06-14 18:15:29 +0800

    Update PictureTestServiceImpl.java

--- src/main/java/com/leiduoduo/abss/service/PictureTestServiceImpl.java
+++ src/main/java/com/leiduoduo/abss/service/PictureTestServiceImpl.java
@@ -15,8 +15,8 @@ public class PictureTestServiceImpl implements PictureTestService {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         String endpoint = "oss-cn-beijing.aliyuncs.com";
         // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录RAM控制台创建RAM账号。
-        String accessKeyId = "LTAI5tDwaUVngnJWmQ1mp4iJ";
-        String accessKeySecret = "eX3QnROGFnlhaw6LBYPFFAc1ExlQlN";
+        String accessKeyId = "*****aUVngnJWmQ1mp4iJ";
+        String accessKeySecret = "******haw6LBYPFFAc1ExlQlN";
         String bucketName = "shudianbucket-guangzhou";
         // <yourObjectName>上传文件到OSS时需要指定包含文件后缀在内的完整路径，例如abc/efg/123.jpg。
         String objectName = System.currentTimeMillis()+".jpg";
